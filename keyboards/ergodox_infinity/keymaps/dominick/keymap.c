88f729fb4f8479c73c5cffa77ee2906768e1dc38
Dominick Pham <dominip@amazon.com>
2020-08-06 21:08:46 -0700

    40% update

--- keyboards/ergodox_infinity/keymaps/dominick/keymap.c
+++ keyboards/ergodox_infinity/keymaps/dominick/keymap.c
@@ -30,13 +30,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  End |  App | LALT | Ctrl |                                       |  '"  |  -_  |   FN |  Alt | Ctrl |
+ *   |Grv/L1|  End |  App | LGUI | Ctrl |                                       |  '"  |  -_  |   FN |  Alt | Ctrl |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Pscr | Ins  |       | Mute |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space| LGUI |------|       |------|  RGUI  |BKSPC |
+ *                                 | Space| LALT |------|       |------|  RGUI  |BKSPC |
  *                                 |      |      | Del  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -48,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,          KC_Q,        KC_W,   KC_E,    KC_R,   KC_T,   KC_LBRC,
         FN,              KC_A,        KC_S,   KC_D,    KC_F,   KC_G,
         KC_LSFT,         CTL_T(KC_Z), KC_X,   KC_C,    KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV), KC_END,     KC_APP, KC_LALT, KC_LCTL,
+        LT(SYMB,KC_GRV), KC_END,     KC_APP, KC_LGUI, KC_LCTL,
                                                     KC_PSCR,  KC_INS,
                                                              KC_HOME,
-                                               KC_SPC,KC_LGUI,KC_DEL,
+                                               KC_SPC,KC_LALT,KC_DEL,
         // right hand
              KC_RGHT,     KC_6,   KC_7,  KC_8,    KC_9,   KC_0,    KC_EQL,
              KC_RBRC,     KC_Y,   KC_U,  KC_I,    KC_O,   KC_P,    KC_BSLS,
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        _______, KC_UP,   KC_LBRC, KC_RBRC, _______, KC_PSCR, KC_HOME,
                 KC_DOWN, KC_LCBR, KC_RCBR, _______, KC_INS,  KC_END,
        _______, KC_AMPR, _______, _______, _______, _______, _______,
-                         _______, _______, _______, _______, _______,
+                         KC_MUTE, KC_MPLY, _______, KC_VOLU, KC_VOLD,
        RGB_TOG, RGB_SLD,
        _______,
        _______, _______, KC_SPC
