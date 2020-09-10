8a79f97f51cad4ff112d3593ca48c4ed5cf2616d
Dominick Pham <dominip@amazon.com>
2020-09-09 18:14:32 -0700

    left alt

--- keyboards/ut472/keymaps/dominick/keymap.c
+++ keyboards/ut472/keymaps/dominick/keymap.c
@@ -50,7 +50,7 @@ LAYOUT(
   KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   LT3_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_LCTL, KC_APP, KC_LALT, KC_LGUI,  MO(1),      KC_SPC,     KC_QUOT,      MO(2), KC_MINS,  KC_EQL, KC_BSLS
+  KC_LCTL, KC_APP, KC_LGUI, KC_LALT,  MO(1),      KC_SPC,     KC_QUOT,      MO(2), KC_MINS,  KC_EQL, KC_BSLS
 ),
 
 
