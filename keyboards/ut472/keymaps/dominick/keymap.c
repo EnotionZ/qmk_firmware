515ddefc567f2ca7cafffc8ae8f1aba1b8a287e9
Dominick Pham <dominip@amazon.com>
2020-11-19 17:37:27 -0800

    normal tab

--- keyboards/ut472/keymaps/dominick/keymap.c
+++ keyboards/ut472/keymaps/dominick/keymap.c
@@ -24,10 +24,13 @@ enum layer_names {
 };
 
 enum custom_keycodes {
-  PWD1P = SAFE_RANGE,
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  LT3TAB,
+  PWD1P,
   PWDAA,
   PWDME,
-  LT3_TAB
 };
 
 
@@ -46,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------'
    */
 
-LAYOUT(
+[_QWERTY] = LAYOUT(
   KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  LT3_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_LCTL, KC_APP, KC_LGUI, KC_LALT,  MO(1),      KC_SPC,     MO(2),      KC_QUOT, KC_MINS,  KC_EQL, KC_BSLS
+  KC_LCTL, KC_APP, KC_LGUI, KC_LALT,  LOWER,      KC_SPC,     RAISE,      KC_QUOT, KC_MINS,  KC_EQL, KC_BSLS
 ),
 
 
@@ -67,11 +70,11 @@ LAYOUT(
    * `-------------------------------------------------------------------------'
    */
 
-LAYOUT( /* Left */
+[_LOWER] = LAYOUT( /* Left */
   KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,  KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_MINS,  KC_EQL, KC_UNDS, KC_PLUS, KC_TILD,
-  _______, _______, _______, _______, _______,     KC_BSPC,     _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  _______, _______, _______, _______, LOWER,     KC_BSPC,     RAISE, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 
@@ -88,11 +91,11 @@ LAYOUT( /* Left */
    * `-------------------------------------------------------------------------'
    */
 
-LAYOUT( /* Right */
-  KC_GESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,  KC_LPRN, KC_RPRN, KC_DEL,
+[_RAISE] = LAYOUT( /* Right */
+  KC_GRV,  KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,  KC_LPRN, KC_RPRN, KC_DEL,
   _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, KC_QUOT, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
-  _______, _______, _______, _______, KC_ENT,     KC_BSPC,     KC_MPLY, _______, KC_VOLD, KC_VOLU, KC_MUTE
+  _______, _______, _______, _______, LOWER,     KC_BSPC,     RAISE, _______, KC_VOLD, KC_VOLU, KC_MUTE
 
 ),
 
@@ -111,13 +114,23 @@ LAYOUT( /* Right */
    */
 
 [_FUNCTION] = LAYOUT( /* Tab */
-  KC_GRV,  _______, KC_UP, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, KC_PSCR, RESET,
-  _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, KC_LCBR, KC_RCBR, _______, RGB_MOD, RGB_TOG,
+  _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG,
   KC_CAPS, _______, _______,  PWDME, PWDAA, PWD1P, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______,  _______, KC_ENT,     KC_BSPC,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  _______, _______, _______,  _______, LOWER,     KC_BSPC,  RAISE, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 };
 
+
+
+// Setting LT3TAB layer RGB back to default
+void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
+  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
+    layer_on(layer3);
+  } else {
+    layer_off(layer3);
+  }
+}
 
 static bool fn_down = false;
 static bool mod_down = false;
@@ -139,7 +152,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
 
-    case LT3_TAB:
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer_RGB(_LOWER, _RAISE, _FUNCTION);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer_RGB(_LOWER, _RAISE, _FUNCTION);
+      }
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer_RGB(_LOWER, _RAISE, _FUNCTION);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer_RGB(_LOWER, _RAISE, _FUNCTION);
+      }
+
+    case LT3TAB:
       // if modifier down, treat as tab
       if(mod_down) {
         if(record->event.pressed) {
@@ -156,7 +187,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           layer_off(_FUNCTION);
 
           // previous keydown was fn/tab
-          if (prev_keydown == LT3_TAB) {
+          if (prev_keydown == LT3TAB) {
             tap_code(KC_TAB);
             prev_keydown = keycode;
           }
