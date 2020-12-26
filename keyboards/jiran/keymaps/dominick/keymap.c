b53c51d31496520b0d7ea6f5659ed78d32230e9e
Dominick Pham <dominip@amazon.com>
2020-12-26 15:45:57 -0800

    control tab mod trigger

--- keyboards/jiran/keymaps/dominick/keymap.c
+++ keyboards/jiran/keymaps/dominick/keymap.c
@@ -9,7 +9,7 @@ enum layers {
 };
 
 enum custom_keycodes {
-  RGBRST,
+  RGBRST = SAFE_RANGE,
   PWD1P,
   PWDAA,
   PWDME,
@@ -20,21 +20,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer: QWERTY
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |RAIS/ESC|   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  | \   |
+ * |  GESC  |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  | \   |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Ctrl/BS |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
+ * |Ctrl/TAB|   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  Enter |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |LShift|LShift|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? |  - _   |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | GUI  | Del  | Enter| Space| Esc  |  | Enter| Space| Tab  | Bksp | AltGr|
- *                        |      |      | Alt  | Lower| Raise|  | Lower| Raise|      |      |      |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      | Esc  |  | Del  |      |   N  |   M  | ,  < | . >  | /  ? |  Shift |
+ * `----------------------+------+------+------+ SPC  +------|  |------+ BSPC +------+------+------+----------------------'
+ *                        | GUI  | Del  | Lower|      | LCTL |  | Esc  |      | Raise| RGUI | RALT |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                                   KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
-     KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                                                  KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_GRV, KC_TILD,                   KC_DEL, KC_RCTL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                     KC_MUTE, KC_ESC, KC_LALT, LT(_LOWER, KC_SPC), KC_SPACE, KC_BSPC, LT(_RAISE, KC_BSPC), KC_RGUI, KC_RALT, KC_MPLY
+     KC_GESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                                     KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+     MT(MOD_LCTL, KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G,                        KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_SPACE, KC_ESC,  KC_DEL, KC_BSPC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+             KC_MUTE, KC_LALT, MO(_LOWER),  KC_SPACE, KC_LCTL, KC_ESC, KC_BSPC, MO(_RAISE), KC_RGUI, KC_RALT
     ),
 /*
  * Lower Layer: Number keys, media, navigation
@@ -51,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_LOWER] = LAYOUT(
-      KC_ESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+      KC_GRV,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
       _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                        KC_F6, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
       _______, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, _______, _______, _______, _______, KC_F12, KC_MINS, KC_EQL,  KC_UNDS, KC_PLUS, _______,
                                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
@@ -93,8 +92,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_ADJUST] = LAYOUT(
       _______, RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD,                                      _______, _______, _______, _______, _______, _______,
-      _______, _______, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,                                     _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, PWDME,   PWDAA,   PWD1P,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      PWD1P,   _______, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,                                     _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, PWDME,   PWDAA,   _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 // /*
