120532572f1d3d84344be0d472fcee6083e637bb
Dominick Pham <dominip@amazon.com>
2021-03-05 13:35:41 -0800

    nyquist MO update

--- keyboards/keebio/nyquist/keymaps/dominick/keymap.c
+++ keyboards/keebio/nyquist/keymaps/dominick/keymap.c
@@ -1,16 +1,17 @@
+/**
+ * make keebio/nyquist/rev3:dominick
+ * atmega32u4
+ */
 #include QMK_KEYBOARD_H
 #include "shared.h"
 
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
-#define _LT3TAB 3
+#define _ADJUST 3
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  LT3TAB,
   PWD1P,
   PWDAA,
   PWDME,
@@ -22,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | GESC   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | \  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab/L3 |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | En |
+ * | Tab    |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | En |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Sh |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -30,10 +31,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-   KC_TAB, KC_Q,   KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
-  KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,  KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  KC_GESC, KC_Q,   KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
+  KC_TAB, KC_A,   KC_S,    KC_D,    KC_F,  KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,  KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_RCTL, KC_APP, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_BSPC, RAISE, KC_QUOT, KC_MINS, KC_EQL,  KC_MPLY
+  KC_LCTL, KC_APP, KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, KC_BSPC, MO(_RAISE), KC_QUOT, KC_MINS, KC_EQL,  KC_MPLY
 ),
 
 /* Lower
@@ -48,10 +49,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+  KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,  KC_MINS, KC_EQL,  KC_UNDS, KC_PLUS, KC_TILD,
-  KC_ENT,  _______, _______, _______, LOWER,   KC_ENT, _______, RAISE,   _______, KC_SCROLLLOCK, KC_PAUSE, _______
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,  KC_MINS, KC_EQL,  KC_UNDS, KC_PLUS, _______,
+  KC_ENT,  _______, _______, _______, MO(_LOWER),   KC_ENT, _______, MO(_RAISE),   _______, KC_SCROLLLOCK, KC_PAUSE, _______
 ),
 
 /* Raise
@@ -66,10 +67,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12(
-  KC_GRV,  KC_EXLM,  KC_AT,  KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_ESC,
+  KC_GRV,  KC_EXLM,  KC_AT,  KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_INSERT,
   _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_QUOT, KC_MPLY,
   _______, _______, _______, _______, _______, _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_MOD, RGB_TOG, _______,
-  _______, _______, _______, _______, LOWER,   KC_ENT,  KC_SPC,    RAISE, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE
+  _______, _______, _______, _______, MO(_LOWER),   KC_ENT,  KC_SPC,    MO(_RAISE), KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE
 ),
 
 /* Function
@@ -83,88 +84,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      | Reset|      |Delete| Bksp | Enter|      |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_LT3TAB] = LAYOUT_ortho_4x12(
-  KC_GRV,   _______,   KC_UP,  _______, _______, _______, KC_MUTE, KC_LBRC, KC_RBRC, _______, KC_PSCR, KC_INSERT,
+[_ADJUST] = LAYOUT_ortho_4x12(
+  KC_GRV,   _______,   KC_UP,  _______, _______, _______, KC_MUTE, KC_LBRC, KC_RBRC, _______, KC_PSCR, RESET,
   _______,  KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_VOLU, KC_LCBR, KC_RCBR, _______, _______, _______,
   KC_CAPS,  _______, _______,    PWDME,   PWDAA,   PWD1P, KC_VOLD, KC_MINS,  KC_EQL, _______, _______, _______,
-  _______,    RESET, _______,  _______,   LOWER, _______, _______,   RAISE, _______, _______, _______, _______
+  _______,  _______, _______,  _______,   MO(_LOWER), _______, _______,   MO(_RAISE), _______, _______, _______, _______
 )
-
 };
 
-// Setting LT3TAB layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-    layer_on(layer3);
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  if(IS_LAYER_ON(_LOWER) || IS_LAYER_ON(_RAISE)) {
+    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
   } else {
-    layer_off(layer3);
+    return state;
   }
 }
 
-static bool fn_down = false;
-static bool mod_down = false;
-static uint16_t prev_keydown = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
-  if(record->event.pressed) {
-    prev_keydown = keycode;
-  }
-
   switch (keycode) {
-    case KC_LGUI:
-    case KC_LALT:
-      if(record->event.pressed) {
-        mod_down = true;
-      } else {
-        mod_down = false;
-      }
-      break;
-
-    case LT3TAB:
-      // if modifier down, treat as tab
-      if(mod_down) {
-        if(record->event.pressed) {
-          register_code(KC_TAB);
-        } else {
-          unregister_code(KC_TAB);
-        }
-
-      // modifier is not down, treat as function
-      } else {
-        if(record->event.pressed) {
-          layer_on(_LT3TAB);
-        } else {
-          layer_off(_LT3TAB);
-
-          // previous keydown was fn/tab
-          if (prev_keydown == LT3TAB) {
-            tap_code(KC_TAB);
-            prev_keydown = keycode;
-          }
-        }
-        fn_down = record->event.pressed;
-      }
-      break;
-
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _LT3TAB);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _LT3TAB);
-      }
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _LT3TAB);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _LT3TAB);
-      }
-      return false;
 
     case PWD1P:
       if (record->event.pressed) {
