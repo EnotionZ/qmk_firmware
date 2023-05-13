78363eea1e79bf3aa22f7f1048618b6fc7ca7ce5
Dominick Pham <dominick.pham@gmail.com>
2023-05-12 19:45:23 -0700

    spc for ctl, bspc for gui

--- keyboards/handwired/skeletal/4x6/keymaps/dominick/keymap.c
+++ keyboards/handwired/skeletal/4x6/keymaps/dominick/keymap.c
@@ -24,21 +24,22 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_GUI_ALT]  = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, KC_LALT),
 };
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
-#define ADJUST MO(_ADJUST)
-#define CTLTAB MT(MOD_LCTL, KC_TAB)
-#define SLSHGUI MT(MOD_LGUI, KC_SLSH)
+#define LOWER   MO(_LOWER)
+#define RAISE   MO(_RAISE)
+#define ADJUST  MO(_ADJUST)
 #define TD_ALTG TD(TD_ALT_GUI)
 #define TD_GUIA TD(TD_GUI_ALT)
+#define GUIBSPC MT(MOD_RGUI, KC_BSPC)
+#define CTLTAB  MT(MOD_LCTL, KC_TAB)
+#define CTLSPC  MT(MOD_LCTL, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
       KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
       CTLTAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT, SLSHGUI, KC_RSFT,
-                        KC_MUTE,  LOWER,  KC_SPC,  TD_GUIA,     TD_ALTG, KC_BSPC, RAISE,    _______
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                        KC_MUTE,  LOWER,  CTLSPC,  TD_GUIA,     TD_ALTG, GUIBSPC, RAISE,    _______
   ),
 
 
@@ -46,14 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
       KC_TAB,  _______, _______, _______, KC_ENT,  PWD1P,       _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
       _______, KC_BSPC, KC_DEL,  _______, PWDME,   PWDAA,       _______, KC_MINS, KC_EQL,  KC_UNDS, _______, _______,
-                        _______,  LOWER,  _______, _______,     _______, _______, RAISE,   _______
+                        _______,  LOWER,  _______, _______,     _______, KC_BSPC, RAISE,   _______
   ),
 
   [_RAISE] = LAYOUT(
       KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
       _______, _______, _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, KC_MUTE,
       _______, _______, _______, _______, _______, _______,     KC_MPRV, KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT, _______,
-                        _______, LOWER,   _______, _______,     _______, _______, RAISE,   _______
+                        _______, LOWER,   KC_SPC,  _______,     _______, _______, RAISE,   _______
   ),
 
   [_ADJUST] = LAYOUT(
