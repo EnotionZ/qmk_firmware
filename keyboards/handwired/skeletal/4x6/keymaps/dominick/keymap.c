708fcc1c30d680361ad7ccaea0504598a1c4b064
Dominick Pham <dominick.pham@gmail.com>
2023-05-16 22:52:31 -0700

    control layer

--- keyboards/handwired/skeletal/4x6/keymaps/dominick/keymap.c
+++ keyboards/handwired/skeletal/4x6/keymaps/dominick/keymap.c
@@ -5,7 +5,8 @@ enum layers {
   _QWERTY = 0,
   _LOWER,
   _RAISE,
-  _ADJUST
+  _ADJUST,
+  _CTRL,
 };
 
 enum custom_keycodes {
@@ -17,33 +18,31 @@ enum custom_keycodes {
 #define LOWER   MO(_LOWER)
 #define RAISE   MO(_RAISE)
 #define ADJUST  MO(_ADJUST)
+#define CTRLMDE TG(_CTRL)
 #define GUIBSPC MT(MOD_RGUI, KC_BSPC)
 #define CTLSCLN MT(MOD_LCTL, KC_SCLN)
-#define CTLSPC  MT(MOD_LCTL, KC_SPC)
 #define CTLTAB  MT(MOD_LCTL, KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
   [_QWERTY] = LAYOUT(
-      KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
-      CTLTAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,   CTLSCLN, KC_ENT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+      KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+      CTLTAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    CTLSCLN, KC_ENT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                         KC_MUTE, LOWER,   KC_SPC,  KC_LGUI,     KC_RALT, GUIBSPC, RAISE,   _______
   ),
 
-
   [_LOWER] = LAYOUT(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-      KC_TAB,  _______, _______, KC_ENT,  KC_BTN1, KC_BTN2,     _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
-      _______, KC_BSPC, KC_DEL,  PWDME,   PWDAA,   PWD1P,       _______, KC_MINS, KC_EQL,  KC_UNDS, _______, _______,
-                        _______, LOWER,   _______, _______,     KC_BSPC, _______, RAISE,   _______
+      KC_TAB,  _______, _______, KC_ENT,  KC_BTN1, PWD1P,       _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
+      KC_TRNS, KC_BSPC, KC_DEL,  CTRLMDE, PWDME,   PWDAA,       _______, KC_MINS, KC_EQL,  KC_UNDS, _______, _______,
+                        _______, LOWER,   KC_TRNS, _______,     KC_BSPC, KC_TRNS, RAISE,   _______
   ),
 
   [_RAISE] = LAYOUT(
       KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
       _______, _______, _______, _______, KC_BTN1, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, KC_MUTE,
       _______, _______, _______, _______, _______, _______,     KC_MPRV, KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT, _______,
-                        _______, LOWER,   KC_LCTL, _______,     _______, KC_BSPC, RAISE,   _______
+                        _______, LOWER,   KC_LCTL, _______,     _______, KC_TRNS, RAISE,   _______
   ),
 
   [_ADJUST] = LAYOUT(
@@ -51,6 +50,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_INS,  KC_F11,      KC_F12,  KC_PGDN, KC_PGUP, RGB_MOD, _______, _______,
       KC_CAPS, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
                         _______, LOWER,   _______, _______,     _______, _______, RAISE,   _______
+  ),
+
+  [_CTRL] = LAYOUT(
+      _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+      KC_TAB,  _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+                        _______, LOWER,   _______, KC_LCTL,     _______, _______, RAISE,   _______
   ),
 };
 
--- keyboards/handwired/skeletal/4x6/keymaps/dominick/rules.mk
+++ keyboards/handwired/skeletal/4x6/keymaps/dominick/rules.mk
@@ -1,6 +1,5 @@
 ENCODER_ENABLE    = yes
 THUMBSTICK_ENABLE = yes
-TAP_DANCE_ENABLE  = no
 
 ifeq ($(strip $(THUMBSTICK_ENABLE)), yes)
     POINTING_DEVICE_ENABLE = yes
