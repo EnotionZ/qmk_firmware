fac4852607e203908b0ff7b7d85282dfdb204cfb
Dominick Pham <dominip@amazon.com>
2023-06-21 11:53:33 -0700

    kyria update

--- keyboards/kyria/keymaps/dominick/keymap.c
+++ keyboards/kyria/keymaps/dominick/keymap.c
@@ -36,10 +36,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_GESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                                     KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
-     CTLTAB,  KC_A, KC_S, KC_D, KC_F, KC_G,                                     KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
-     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BSPC, ADJUST,  KC_APP,  KC_DEL,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                    KC_END, KC_LGUI, LOWER, KC_SPC,  KC_LALT, KC_RGUI, KC_BSPC, RAISE, KC_RALT, KC_MUTE
+     KC_GESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                                      KC_Y,  KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
+     KC_TAB,  KC_A, KC_S, KC_D, KC_F, KC_G,                                      KC_H,  KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT,
+     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,  KC_BSPC, ADJUST,  KC_EQL,  KC_DEL,  KC_N,  KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                    KC_MUTE, KC_LGUI, LOWER, KC_SPC,  KC_LCTL, KC_MINS, KC_BSPC, RAISE, KC_RALT, KC_END
     ),
 
 /*
@@ -57,10 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_LOWER] = LAYOUT(
-     KC_GRV,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                                         KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_INS,
-     KC_TAB,  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                        KC_F11, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
-     _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,  _______, _______, _______, _______,  KC_F12, KC_MINS, KC_EQL,  KC_UNDS, KC_PLUS, _______,
-                        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_INS,
+     KC_TAB,  _______, _______, KC_ENT,  KC_INS,  PWD1P,                                       _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
+     _______, KC_BSPC, KC_DEL,  _______, PWDME,   PWDAA,   _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_UNDS, KC_PLUS, _______,
+                       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
 /*
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_RAISE] = LAYOUT(
      KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_DEL,
-     _______, _______, _______, _______, _______, _______,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_QUOT, _______,
-     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______,
+     _______, _______, _______, _______, _______, _______,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_QUOT, KC_MUTE,
+     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MPRV,  KC_MNXT, _______,
                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -99,10 +99,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_ADJUST] = LAYOUT(
-      _______, RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, _______,                                    KC_PSCR, KC_SCRL, KC_PAUS, _______, _______, RESET,
-      _______, RGB_MOD, RGB_SAD, RGB_HUD, RGB_VAD, _______,                                    _______, KC_PGDN, KC_PGUP, _______, _______, _______,
-      KC_CAPS, _______, _______, PWDME,   PWDAA,   PWD1P,   KC_DEL, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                 _______, _______, _______, KC_ENT, _______, _______, _______, _______, _______, _______
+     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RESET,
+     _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_INS,  KC_F11,                                      KC_F12,  KC_PGDN, KC_PGUP, RGB_MOD, _______, RGB_TOG,
+     KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
 // /*
@@ -209,15 +209,15 @@ bool oled_task_user(void) {
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 1) { /* right encoder */
-      if (layer_state_is(_LOWER)) {
+    if (index == 0) { /* left encoder */
+      if (layer_state_is(_RAISE)) {
         // scroll lock and pause controls brightness for active display on Mac
         tap_code(!clockwise ? KC_PAUSE : KC_SCROLLLOCK);
       } else {
         tap_code(!clockwise ? KC_VOLU : KC_VOLD);
       }
-    } else if (index == 0) { /* left encoder */
-      if (layer_state_is(_RAISE)) {
+    } else if (index == 1) { /* right encoder */
+      if (layer_state_is(_LOWER)) {
         // windows brightness
         tap_code(clockwise ? KC_BRIGHTNESS_DOWN : KC_BRIGHTNESS_UP);
       } else {
