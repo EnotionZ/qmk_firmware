0ab2c785deda012c70fb1035f8a7ee8aa5d8b1f8
Dominick Pham <dominip@amazon.com>
2023-07-11 09:19:09 -0700

    pwdev

--- keyboards/libra_mini/keymaps/dominick/keymap.c
+++ keyboards/libra_mini/keymaps/dominick/keymap.c
@@ -2,7 +2,6 @@
 #include "joystick.h"
 #include "analog.h"
 #include "shared.h"
-#include "time.h"
 
 enum layers {
   _QWERTY = 0,
@@ -15,7 +14,8 @@ enum custom_keycodes {
   PWD1P = SAFE_RANGE,
   PWDAA,
   PWDME,
-  TD_LWRG,
+  PWDEV,
+  TD_LWRC,
 };
 
 #define LOWER   MO(_LOWER)
@@ -31,28 +31,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  KC_BSLS,
       CTLTAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_BALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-      KC_LCTL, KC_LGUI,          TD_LWRG, KC_SPC,                        GUIBSPC, RAISE,                     KC_RALT, KC_RGUI
+      KC_LALT, KC_LGUI,          TD_LWRC, KC_SPC,                        GUIBSPC, RAISE,                     KC_RALT, KC_RGUI
   ),
 
   [_LOWER] = LAYOUT(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______,
-      KC_TAB,  _______, _______, KC_INS,  KC_ENT,  PWD1P,       _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
+      KC_TAB,  PWDEV,   _______, KC_INS,  KC_ENT,  PWD1P,       _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
       KC_TRNS, KC_BSPC, KC_DEL,  KC_BTN1, PWDME,   PWDAA,       _______, KC_BSPC, KC_MINS, KC_EQL,  KC_UNDS, _______, _______,
-      _______, _______,          TD_LWRG, _______,                       _______, RAISE,                     _______, _______
+      _______, _______,          TD_LWRC, KC_SPC,                        _______, RAISE,                     _______, _______
   ),
 
   [_RAISE] = LAYOUT(
       KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, KC_DEL,
       _______, _______, _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, _______,
       _______, _______, _______, _______, _______, _______,     _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MUTE,
-      _______, _______,          TD_LWRG, KC_LCTL,                       _______, RAISE,                     _______, _______
+      _______, _______,          TD_LWRC, KC_SPC,                        _______, RAISE,                     _______, _______
   ),
 
   [_ADJUST] = LAYOUT(
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, RESET,
       _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_INS,  KC_F11,      KC_F12,  KC_PGDN, KC_PGUP, _______, _______, _______,
       KC_CAPS, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
-      _______, _______,          TD_LWRG, _______,                       _______, RAISE,                     _______, _______
+      _______, _______,          TD_LWRC, KC_SPC,                        _______, RAISE,                     _______, _______
   ),
 };
 
@@ -176,7 +176,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
 
-    case TD_LWRG:
+    case PWDEV:
+      if (keydown) {
+        send_string_with_delay_P(PSTR(CPWDEV SS_TAP(X_ENT)), SEND_STR_DELAY);
+      }
+      break;
+
+    case TD_LWRC:
       handle_tapdance(record, LOWER, true, KC_LCTL, false);
       break;
 
