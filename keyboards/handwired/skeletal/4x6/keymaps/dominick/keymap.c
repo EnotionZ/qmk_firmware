a0172972a7ea8fcc931fb6162e11a4e0f1e1899d
Dominick Pham <dominip@amazon.com>
2023-08-01 08:38:01 -0700

    td with 2 modifiers not good

--- keyboards/handwired/skeletal/4x6/keymaps/dominick/keymap.c
+++ keyboards/handwired/skeletal/4x6/keymaps/dominick/keymap.c
@@ -19,6 +19,7 @@ enum custom_keycodes {
   LWR_GUI,
   TD_ALTQ,
   GUI_CTL,
+  TD_LWRC,
 };
 
 #define LOWER   MO(_LOWER)
@@ -35,28 +36,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
       CTLTAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_MUTE, LOWER,   KC_SPC,  GUI_CTL,     KC_LALT, GUIBSPC, RAISE,   _______
+                        KC_MUTE, TD_LWRC, KC_SPC,  KC_LGUI,     KC_LALT, GUIBSPC, RAISE,   _______
   ),
 
   [_LOWER] = LAYOUT(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
       KC_TAB,  PWDEV,   _______, KC_INS,  KC_ENT,  PWD1P,       GUI_ALT, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
       KC_TRNS, KC_BSPC, KC_DEL,  KC_BTN1, PWDME,   PWDAA,       KC_BSPC, KC_MINS, KC_EQL,  KC_UNDS, CTRLMDE, _______,
-                        _______, LOWER,   KC_SPC,  _______,     _______, KC_BSPC, RAISE,   _______
+                        _______, TD_LWRC, KC_SPC,  _______,     _______, KC_BSPC, RAISE,   _______
   ),
 
   [_RAISE] = LAYOUT(
       KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
       _______, _______, _______, _______, KC_BTN1, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, _______,
       _______, _______, _______, CTRLMDE, _______, _______,     KC_MPLY, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MUTE,
-                        _______, LOWER,   KC_SPC,  _______,     _______, GUIBSPC, RAISE,   _______
+                        _______, TD_LWRC, KC_SPC,  _______,     _______, GUIBSPC, RAISE,   _______
   ),
 
   [_ADJUST] = LAYOUT(
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RESET,
       _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_INS,  KC_F11,      KC_F12,  KC_PGDN, KC_PGUP, RGB_MOD, _______, RGB_TOG,
       KC_CAPS, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
-                        _______, LOWER,   KC_SPC,  _______,     _______, GUIBSPC, RAISE,   _______
+                        _______, TD_LWRC, KC_SPC,  _______,     _______, GUIBSPC, RAISE,   _______
   ),
 
   [_CTRL] = LAYOUT(
@@ -190,6 +191,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         unregister_code(KC_RALT);
         layer_off(_LOWER);
       }
+      break;
+
+    case TD_LWRC:
+      handle_tapdance(record, LOWER, true, KC_LCTL, false);
       break;
 
     case GUI_CTL:
