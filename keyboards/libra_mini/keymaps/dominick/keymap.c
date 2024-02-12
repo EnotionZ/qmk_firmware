0bcba6aa8b259a6a41accc962e824d03627a52e7
Dominick Pham <dominick.pham@gmail.com>
2024-02-11 16:55:45 -0800

    vsclose

--- keyboards/libra_mini/keymaps/dominick/keymap.c
+++ keyboards/libra_mini/keymaps/dominick/keymap.c
@@ -17,6 +17,7 @@ enum custom_keycodes {
   PWDME,
   PWDEV,
   TD_LWRC,
+  VSCLOSE,
 };
 
 #define TGMAC   TG(_MAC)
@@ -33,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  KC_BSLS,
       KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-      KC_LCTL, KC_LALT,          TD_LWRC, KC_SPC,                        KC_BSPC, RAISE,                     KC_RGUI, KC_RALT
+      KC_LCTL, KC_LALT,          LOWER,   KC_SPC,                        KC_BSPC, RAISE,                     KC_RGUI, KC_RALT
   ),
 
   [_MAC] = LAYOUT(
@@ -45,23 +46,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______,
-      KC_TAB,  PWDEV,   _______, KC_INS,  KC_ENT,  PWD1P,       _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
+      KC_TAB,  PWDEV,   VSCLOSE, KC_INS,  KC_ENT,  PWD1P,       _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
       KC_TRNS, KC_BSPC, KC_DEL,  KC_BTN1, PWDME,   PWDAA,       _______, _______, KC_MINS, KC_EQL,  KC_UNDS, _______, _______,
-      _______, _______,          TD_LWRC, KC_SPC,                        _______, RAISE,                     _______, _______
+      _______, _______,          LOWER,   KC_SPC,                        _______, RAISE,                     _______, _______
   ),
 
   [_RAISE] = LAYOUT(
       KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, KC_DEL,
       _______, _______, _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, _______,
       _______, _______, _______, _______, _______, _______,     _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MUTE,
-      _______, _______,          TD_LWRC, KC_SPC,                        _______, RAISE,                     _______, _______
+      _______, _______,          LOWER,   KC_SPC,                        _______, RAISE,                     _______, _______
   ),
 
   [_ADJUST] = LAYOUT(
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, RESET,
       _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_INS,  KC_F11,      KC_F12,  KC_PGDN, KC_PGUP, _______, _______, _______,
       KC_CAPS, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
-      _______, _______,          TD_LWRC, KC_SPC,                        _______, RAISE,                     _______, TGMAC
+      _______, _______,          LOWER,   KC_SPC,                        _______, RAISE,                     _______, TGMAC
   ),
 };
 
@@ -193,6 +194,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case TD_LWRC:
       handle_tapdance(record, LOWER, true, KC_LCTL, false);
+      break;
+
+    case VSCLOSE:
+      if(keydown) {
+        register_code(KC_LCTL);
+        tap_code(KC_W);
+        unregister_code(KC_LCTL);
+        tap_code(KC_Q);
+      }
       break;
 
     case KC_LSFT:;
