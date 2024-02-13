b3e70b718c91bc30a9ff44a3aacc9f18c0562742
Dominick Pham <dominick.pham@gmail.com>
2024-02-12 18:39:32 -0800

    vclose

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/dominick/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dominick/keymap.c
@@ -16,6 +16,7 @@ enum custom_keycodes {
   PWDEV,
   CTLTB,
   MACDEL,
+  VSCLOSE,
 };
 
 
@@ -53,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
      KC_TAB,  PWDEV,   MACDEL,  KC_INS,  KC_ENT,  PWD1P,         _______, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
-     KC_TRNS, KC_BSPC, KC_DEL,  _______,  PWDME,   PWDAA,         _______, KC_MINS, KC_EQL,  KC_UNDS, KC_PLUS, _______,
+     KC_TRNS, KC_BSPC, KC_DEL,  VSCLOSE, PWDME,   PWDAA,         _______, KC_MINS, KC_EQL,  KC_UNDS, KC_PLUS, _______,
                        _______, _______,                                           _______, _______,
                                          _______, KC_TRNS,       KC_TRNS, _______,
                                          _______, _______,       _______, KC_SPC,
@@ -155,6 +156,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case PWDEV:
       if (record->event.pressed) {
         send_string_with_delay_P(PSTR(CPWDEV SS_TAP(X_ENT)), SEND_STR_DELAY);
+      }
+      break;
+
+    case VSCLOSE:
+      if(keydown) {
+        register_code(KC_LCTL);
+        tap_code(KC_W);
+        unregister_code(KC_LCTL);
+        tap_code(KC_Q);
       }
       break;
 
