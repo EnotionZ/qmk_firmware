086a63fa9d2b0efa0d2ec618f1c10a2964b2fcd2
Dominick Pham <dominick.pham@gmail.com>
2024-02-13 16:19:26 -0800

    close all

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/dominick/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dominick/keymap.c
@@ -3,6 +3,7 @@
 
 enum layers {
   _QWERTY = 0,
+  _MAC,
   _FN,
   _LOWER,
   _RAISE,
@@ -17,11 +18,13 @@ enum custom_keycodes {
   CTLTB,
   MACDEL,
   VSCLOSE,
+  VSCLALL,
 };
 
-
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
+#define TGMAC  TG(_MAC)
+#define TGFN   TG(_FN)
+#define LOWER  MO(_LOWER)
+#define RAISE  MO(_RAISE)
 #define ADJUST MO(_ADJUST)
 #define CTLTAB MT(MOD_LCTL, KC_TAB)
 
@@ -32,10 +35,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
      KC_RCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                       KC_LALT, KC_LGUI,                                           KC_RGUI, KC_RALT,
+                       KC_LGUI, KC_LALT,                                           KC_RALT, KC_RGUI,
                                          LOWER,   KC_TRNS,       KC_TRNS, RAISE,
                                          KC_SPC,  KC_MUTE,       KC_EQL,  KC_BSPC,
                                          KC_RGUI, KC_LCTL,       KC_MINS, KC_DEL
+  ),
+
+  [_MAC] = LAYOUT_5x6(
+     _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______,
+                       KC_LALT, KC_LGUI,                                           KC_RGUI, KC_RALT,
+                                         _______, _______,       _______, _______,
+                                         _______, _______,       _______, _______,
+                                         _______, _______,       _______, _______
   ),
 
   [_FN] = LAYOUT_5x6(
@@ -65,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR , KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
      KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR , KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
      _______, _______, _______, _______, _______, _______,       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, KC_MUTE,
-     _______, _______, _______, _______, _______, _______,       KC_MPLY, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, _______,
+     _______, _______, _______, VSCLALL, _______, _______,       KC_MPLY, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, _______,
                        _______, _______,                                           _______, _______,
                                          _______, KC_TRNS,       KC_TRNS, _______,
                                          _______, _______,       _______, _______,
@@ -75,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = LAYOUT_5x6(
      _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, RESET,
      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-     _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_INS,  KC_F11,        KC_F12,  KC_PGDN, KC_PGUP, _______, _______, _______,
-     KC_CAPS, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______,
+     _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_INS,  KC_F11,        KC_F12,  KC_PGDN, KC_PGUP, _______, _______, TGFN,
+     KC_CAPS, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, TGMAC,
                        _______, _______,                                           _______, _______,
                                          _______, _______,       _______, _______,
                                          _______, _______,       _______, _______,
@@ -160,11 +174,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
 
     case VSCLOSE:
-      if(keydown) {
+      if(record->event.pressed) {
         register_code(KC_LCTL);
         tap_code(KC_W);
         unregister_code(KC_LCTL);
         tap_code(KC_Q);
+      }
+      break;
+
+    case VSCLALL:
+      if(record->event.pressed) {
+        register_code(KC_LCTL);
+        tap_code(KC_K);
+        unregister_code(KC_LCTL);
+        tap_code(KC_W);
       }
       break;
 
